2e741f138c061bb9ff1fa6e6f6d2f2fe764faf73
Augusto <augustocepinheiro@gmail.com>
2019-06-12 14:19:20 -0300

    teste de usabilidade

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
-    private InputField nameInputField;
+   
     public void PlayGame()
     {
-        nameInputField = GameObject.Find("nameInput").GetComponent<InputField>();
-        PlayerPrefs.SetString("playerName", nameInputField.text);
-        PlayerPrefs.Save();
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
 
     }
 
@@ -23,6 +20,6 @@ public class MainMenu : MonoBehaviour
         Application.CancelQuit();
 
     }
-   
+
 
 }
